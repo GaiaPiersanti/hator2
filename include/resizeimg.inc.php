f07fe21e2ba0ab3e560d965bf1ada0a7aba0dd15
armmol-98 <armando.molino@student.univaq.it>
2025-07-12 19:23:09 +0200

    inserita funzione per il resize

---
+++ include/resizeimg.inc.php
@@ -0,0 +1,54 @@
+<?php
+/* questa funzione arrangia l'immagine in modo che prenda quelle dimensioni date
+   possiamo modificarlo in modo che prend apiù tipi di file
+   per poterlo utilizzare dobiamo richiamarlo in un file php*/ */
+function get_thumbnail($file,$width = 300,$height = 300){
+ 
+		if(file_exists($file)){
+		
+			$file_extention = explode(".",$file);
+			$file_extention = end($file_extention);
+			
+			if(strtolower($file_extention) == "jpg" || strtolower($file_extention) == "jpeg"){
+			
+				$thumb = str_replace("." . $file_extention,"_thumb." . $file_extention,$file);
+		 
+		  
+				if(file_exists($thumb)){
+		  
+					return $thumb;
+				 
+				}else{
+				
+					make_new:
+					//create a square thumbnail for display
+				 
+						$this->resize_image_crop($file,$thumb,$width,$height);
+				 
+					
+					if(file_exists($thumb)){
+					
+						return $thumb;
+					
+					}else{
+					
+						return($file);
+						
+					}
+					
+				}
+				
+			}else{
+			
+				return $file;
+			
+			}
+
+		}else{
+		
+			return $file;
+		
+		}
+
+	}
+?>
