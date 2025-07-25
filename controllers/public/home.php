fb6c396be91bbc186e1c4a5e1c70a5c2bbc5a02f
GaiaPiersanti <135056936+GaiaPiersanti@users.noreply.github.com>
2025-07-25 02:19:54 +0200

    banner hero

--- controllers/public/home.php
+++ controllers/public/home.php
@@ -1,64 +1,75 @@
 <?php
-// 1) init (connessione $conn, Template, ecc.)
-
-
-// 2) prendo i banner slider
-$stmt = $conn->prepare("SELECT * FROM banners WHERE tipo = 'slider' ORDER BY ordine");
-$stmt->execute();
-$slides = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-// 3) prendo i banner hero
-$stmt = $conn->prepare("SELECT * FROM banners WHERE tipo = 'hero' ORDER BY ordine");
-$stmt->execute();
-$heros  = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
-
-// 4) costruisco l’HTML dello slider
-$sliderHtml  = '<div class="slider-area slider-style-three">';
-$sliderHtml .=   '<div class="slider-activation owl-carousel">';
-foreach ($slides as $b) {
-    $id    = htmlspecialchars($b['id'],    ENT_QUOTES);
-    $img   = htmlspecialchars($b['img_url'],ENT_QUOTES);
-    $link  = htmlspecialchars($b['link'],   ENT_QUOTES);
-    $text  = htmlspecialchars($b['testo'],  ENT_QUOTES);
-    $label = htmlspecialchars($b['nome'],   ENT_QUOTES);
-
-    $sliderHtml .= "<div class=\"slide align-center-left fullscreen animation-style-{$id}\" style=\"background-image:url('{$img}')\">";
-    $sliderHtml .=   '<div class="slider-progress"></div>';
-    $sliderHtml .=   '<div class="container"><div class="row"><div class="col-lg-12"><div class="slider-content">';
-    if ($text !== '') {
-        $sliderHtml .= "<p style=\"color: black;\">{$text}</p>";
-    }
-    $sliderHtml .=     "<div class=\"slide-btn white-color\"><a href=\"{$link}\">{$label}</a></div>";
-    $sliderHtml .=   '</div></div></div></div>';
-    $sliderHtml .= '</div>';
-}
-$sliderHtml .=   '</div>';
-$sliderHtml .= '</div>';
-
-// 5) costruisco l’HTML dell’hero-banner
-$heroHtml   = '<div class="hero-banner-area">';
-$heroHtml  .=   '<div class="container-fluid"><div class="row">';
-foreach ($heros as $b) {
-    $link = htmlspecialchars($b['link'],   ENT_QUOTES);
-    $img  = htmlspecialchars($b['img_url'],ENT_QUOTES);
-    $alt  = htmlspecialchars($b['nome'],   ENT_QUOTES);
-
-    $heroHtml .= '<div class="col-md-6 col-sm-6 mb-xsm-30">';
-    $heroHtml .=   '<div class="single-banner zoom">';
-    $heroHtml .=     "<a href=\"{$link}\"><img src=\"{$img}\" alt=\"{$alt}\"></a>";
-    $heroHtml .=   '</div>';
-    $heroHtml .= '</div>';
-}
-$heroHtml  .=   '</div></div>';
-$heroHtml  .= '</div>';
 
 // 6) renderizzo il template
 $main = new Template("dtml/hator/frame");
 $main->setContent("page_title", $page_title);
 
+// 1) Prendi tutti i banner di tipo “hero”, ordinati per “ordine”
+$stmt = $conn->prepare("
+  SELECT b.img_url,
+         b.link AS brand_slug,
+         br.id   AS brand_id
+    FROM banners b
+LEFT JOIN brands br ON br.slug = b.link
+   WHERE b.tipo = 'hero'
+ORDER BY b.ordine ASC
+");
+$stmt->execute();
+$banners = $stmt->get_result()->fetch_all(MYSQLI_ASSOC);
+$stmt->close();
+
+// 2) Costruisci un HTML dinamico
+$heroHtml = '<div class="hero-banner-area"><div class="container-fluid"><div class="row">';
+
+foreach ($banners as $index => $ban) {
+    // Decidi la struttura in base alla posizione (index)
+    if ($index === 0) {
+        // primo banner a tutta larghezza col-md-6
+        $heroHtml .= '
+        <div class="col-md-6 col-sm-6 mb-xsm-30 banner-large" style="padding-right: 15px; padding-left: 15px;">
+          <div class="single-banner zoom">
+            <a href="' . htmlspecialchars('index.php?page=shop&brands[]=' . intval($ban['brand_id'])) . '">
+              <img src="' . htmlspecialchars($ban['img_url']) . '" alt="banner-img">
+            </a>
+          </div>
+        </div>';
+    } elseif ($index >= 1 && $index <= 2) {
+        // secondo e terzo banner nella col-md-6 superiore
+        if ($index === 1) {
+            $heroHtml .= '<div class="col-md-6 col-sm-6">
+                           <div class="banner-inner-top"><div class="row">';
+        }
+        $heroHtml .= '
+        <div class="col-md-6 col-sm-6 col-6 banner-small">
+          <div class="single-banner zoom">
+            <a href="' . htmlspecialchars('index.php?page=shop&brands[]=' . intval($ban['brand_id'])) . '">
+              <img src="' . htmlspecialchars($ban['img_url']) . '" alt="banner-img">
+            </a>
+          </div>
+        </div>';
+        if ($index === 2) {
+            $heroHtml .= '</div></div>';
+        }
+    } elseif ($index === 3) {
+        // quarto banner in basso, dentro lo stesso col-md-6 dei banner-small
+        $heroHtml .= '
+          <div class="banner-inner-bottom single-banner zoom banner-wide" style="margin-top: 30px;">
+            <a href="' . htmlspecialchars('index.php?page=shop&brands[]=' . intval($ban['brand_id'])) . '">
+              <img src="' . htmlspecialchars($ban['img_url']) . '" alt="banner-img">
+            </a>
+          </div>
+        </div>';  // chiude il div col-md-6 aperto per i banner-small
+    }
+}
+
+// chiudi i div di row/container
+$heroHtml .= '</div></div></div>';
+
+// 3) Passa al template
+
+
 $body = new Template("dtml/hator/home");
-$body->setContent("slider_section", $sliderHtml);
-$body->setContent("hero_section",   $heroHtml);
+$body->setContent('heroBanner', $heroHtml);
 
 $main->setContent("body", $body->get());
 $main->close();
